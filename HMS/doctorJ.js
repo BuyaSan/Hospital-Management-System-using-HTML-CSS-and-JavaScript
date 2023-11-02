76b0c1dc0e4d8ee18c254a40faaf073f331231d3
Rafael Onwubuya <ralphbuya4@gmail.com>
2023-11-02 14:44:44 +0100

    Code updates and corrections

--- HMS/doctorJ.js
+++ HMS/doctorJ.js
@@ -1,58 +1,47 @@
-function validation()
-{
-  let x = document.forms["dform"]["fname"].value
-  //var x = document.dform.fname.value 
-  if(x == "")
-  {
-  	alert("first name must be filled")
-  	return false
+function validation() {
+  let x = document.forms["dform"]["fname"].value;
+  //var x = document.dform.fname.value
+  if (x == "") {
+    alert("first name must be filled");
+    return false;
   }
 }
 
-function AllowOnlyAlpha(input)
-{
-	var regex = /[^a-z,^A-Z]/gi
-	input.value = input.value.replace(regex,"")
-
+function AllowOnlyAlpha(input) {
+  var regex = /[^a-z,^A-Z]/gi;
+  input.value = input.value.replace(regex, "");
 }
 
-function AllowOnlyNumeric(input)
-{
-	var numeric = /[^0-9]/gi
-	input.value = input.value.replace(numeric,"")
+function AllowOnlyNumeric(input) {
+  var numeric = /[^0-9]/gi;
+  input.value = input.value.replace(numeric, "");
 }
 
-function emailval()
-{
-	var form = document.getElementById("dform")
-	var email = document.getElementById("email").value
-	var text = document.getElementById("text")
-	var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+function emailval() {
+  var form = document.getElementById("dform");
+  var email = document.getElementById("email").value;
+  var text = document.getElementById("text");
+  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-	if(email.match(pattern))
-	{
-		form.classList.add("valid")
-		form.classList.remove("Invalid")
-		text.innerHTML = "Email address is valid"
-	}
-	else
-	{
-		form.classList.remove("valid")
-		form.classList.add("Invalid")
-		text.innerHTML = "Email address is invalid"
-	}
+  if (email.match(pattern)) {
+    form.classList.add("valid");
+    form.classList.remove("Invalid");
+    text.innerHTML = "Email address is valid";
+  } else {
+    form.classList.remove("valid");
+    form.classList.add("Invalid");
+    text.innerHTML = "Email address is invalid";
+  }
 
-	if(email == "")
-	{
-		form.clasList.remove("valid")
-		form.classList.add("Invalid")
-		text.innerHTML = "" 
-
-	}
+  if (email == "") {
+    //corrected classList.remove, it was initially clasList.remove:
+    form.classList.remove("valid");
+    form.classList.add("Invalid");
+    text.innerHTML = "";
+  }
 }
 
-function AllowOnlyNumeric(input)
-{
-	var numeric = /[^0-9]/gi
-	input.value = input.value.replace(numeric,"")
+function AllowOnlyNumeric(input) {
+  var numeric = /[^0-9]/gi;
+  input.value = input.value.replace(numeric, "");
 }
